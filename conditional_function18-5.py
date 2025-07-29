8810046846a3adc08a140de04213c08ebe245c23
pallavipkul <kulkarnipallavi2709@gmail.com>
2025-07-29 13:38:32 +0530

    Add files via upload

---
+++ conditional_function18-5.py
@@ -0,0 +1,78 @@
+# Print numbers from 1 to 10 using a loop.
+# for i in range(1,11):
+#     print(i)
+
+# ===========================================================================================
+# Print all even numbers between 1 and 20.
+# def a(n):
+#     sum=0
+#     for i in range(1,n+1):
+#         if i%2==0:
+#             sum+=i
+#     return sum
+# print(a(20))
+#===========================================================================  
+# Print the characters of the string "Python" one by one using a loop.
+# def a(n):
+#     for i in n:
+#         print(i)
+# print(a("python")) 
+# =============================================================================
+# Print the sum of numbers from 1 to 100.
+# def num(a):
+#     sum=0
+#     for i in range(1,100):
+#         i+=1
+#     return i
+# print(num(100))  
+
+# Print multiplication table of 5 (like 5 x 1 = 5, ... 5 x 10 = 50).
+# def num(n):
+#     for i in range(5,6):
+#         for j in range (1,11):
+#             print(i,"*",j,"=",i*j)
+# print(num(10))
+
+
+      
+# Print all elements of a list using a loop.
+# List: ["apple", "banana", "cherry"]
+
+# L=["apple", "banana", "cherry"]
+# for i in L:
+#     print(i)
+
+# Print only the vowels from the string "education" using a loop.
+
+# def v(s):
+#     vowels = "aeiou"  
+#     for char in s:
+#         if char in vowels:
+#             print(char)
+#     return char
+# print(v("education"))
+
+
+# Count how many times the letter 'a' appears in the string "banana" using a loop.
+# def s(n):
+#     count=0
+#     for i in n:
+#         if i=="a":
+#             count+=1
+#     return count 
+# print(s("banana"))   
+
+
+
+# Print numbers from 10 to 1 using a loop (reverse order).
+
+# for i in range(10,1,-1):
+#     print(i)
+
+# Ask the user to enter 5 numbers using a loop and print their sum.
+
+# num=int(input("enter the value:"))
+# sum=0
+# for i in range(1,num+1):
+#     sum+=i
+# print(sum)
